3525d117707455b2ed86f2ef2724b31a1b82afc8
mgunn001 <mgunnam@cs.odu.edu>
2018-03-05 19:35:55 -0500

    fixed a bug with progress bar stuff

--- tmvis.js
+++ tmvis.js
@@ -1362,6 +1362,9 @@ TimeMap.prototype.createScreenshotsForMementos = function (curCookieClientId,res
       self.createScreenshotForMementoWithPuppeteer(curCookieClientId,memento,callback)
       completedScreenshotCaptures++;
       var value = (completedScreenshotCaptures/noOfThumbnailsSelectedToBeCaptured)*95+5;
+      constructSSE("percentagedone-"+value,curCookieClientId);
+
+
     } ,
     function doneCreatingScreenshots (err) {      // When finished, check for errors
 
