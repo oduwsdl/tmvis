304ca0d2d988fd42f79c0c3fb7ff3ea8d9bbe704
dhruv282 <40037413+dhruv282@users.noreply.github.com>
2019-03-01 19:08:25 -0500

    Update gif.js

--- public/scripts/gif.js
+++ public/scripts/gif.js
@@ -1,26 +1,31 @@
 
-var imagesData_IG = [];
+$(document).ready(function{
+		  
+		  var imagesData = [];
 
-function getImageArray(data){
+		function getImageArray(data){
 
-	imagesData_IG = [];
-		$("#gif .gifContent").empty();
-		$.each(data, function (index, obj) {
-			if($(obj.event_html).attr("src").indexOf("notcaptured") < 0){
-				imagesData_IG.push(obj);
-			}
-		});
+			imagesData = [];
+				$("#gif .gifContent").empty();
+				$.each(data, function (index, obj) {
+					if($(obj.event_html).attr("src").indexOf("notcaptured") < 0){
+						imagesData.push(obj);
+					}
+				});
 
-		var imageLinks = [];
-		for(var i = 0; i<imagesData_IG.length; i++)
-		{
-			imagesData_IG[i].event_link = imageLinks[i];
+				var imageLinks = [];
+				for(var i = 0; i<imagesData.length; i++)
+				{
+					imagesData[i].event_link = imageLinks[i];
+				}
+
+			var interval = document.getElementById("interval").value;
+
+			create(imageLinks, interval);
 		}
+		  
+		  });
 
-	var interval = document.getElementById("interval").value;
-
-	create(imageLinks, interval);
-}
 
 function create(image, int)
 {
