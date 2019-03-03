ef483321917951a885296266fbdf711f2272c05f
dhruv282 <40037413+dhruv282@users.noreply.github.com>
2019-03-03 17:03:28 -0500

    Update gif.js

--- public/scripts/gif.js
+++ public/scripts/gif.js
@@ -1,8 +1,8 @@
 
-$(document).ready(function{
-		  
-		  var imagesData = [];
+var imagesData = [];
+var imageLinks = [];
 
+$(document).ready(function (){
 		function getImageArray(data){
 
 			imagesData = [];
@@ -13,43 +13,45 @@ $(document).ready(function{
 					}
 				});
 
-				var imageLinks = [];
-				for(var i = 0; i<imagesData.length; i++)
-				{
-					imagesData[i].event_link = imageLinks[i];
-				}
+			for(var i = 0; i<imagesData.length; i++)
+			{
+				imagesData[i].event_link = imageLinks[i];
+			}
 
 			var interval = document.getElementById("interval").value;
 
 			create(imageLinks, interval);
+		}
 	
-		  });
 
 
-function create(image, int)
-{
-	gifshot.createGIF({
-    gifWidth: 200,
-    gifHeight: 200,
-    interval: int,
-    numFrames: 10,
-    images: image,
-    frameDuration: 1,
-    fontWeight: 'normal',
-    fontSize: '16px',
-    fontFamily: 'sans-serif',
-    fontColor: '#ffffff',
-    textAlign: 'center',
-    textBaseline: 'bottom',
-    sampleInterval: 10,
-    numWorkers: 4
-	}, function (obj) {
-	    if (!obj.error) {
-	        var image = obj.image, animatedImage = document.createElement('img');
-	        animatedImage.src = image;
-	        document.getElementById("gifApp").appendChild(animatedImage);
-	        //$("#gif div").appendChild(animatedImage);
-	    }
-	});
 
-}
+	function create(image, int)
+	{
+		gifshot.createGIF({
+	    gifWidth: 200,
+	    gifHeight: 200,
+	    interval: int,
+	    numFrames: 10,
+	    images: image,
+	    frameDuration: 1,
+	    fontWeight: 'normal',
+	    fontSize: '16px',
+	    fontFamily: 'sans-serif',
+	    fontColor: '#ffffff',
+	    textAlign: 'center',
+	    textBaseline: 'bottom',
+	    sampleInterval: 10,
+	    numWorkers: 4
+		}, function (obj) {
+		    if (!obj.error) {
+			var image = obj.image, animatedImage = document.createElement('img');
+			animatedImage.src = image;
+			document.getElementById("gifApp").appendChild(animatedImage);
+			//$("#gif div").appendChild(animatedImage);
+		    }
+		});
+
+	}
+});
+
