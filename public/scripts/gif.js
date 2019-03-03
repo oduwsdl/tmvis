533d258e581dbc2af5e88001eacd3ef755b333cc
dhruv282 <40037413+dhruv282@users.noreply.github.com>
2019-03-03 18:33:55 -0500

    Update gif.js

--- public/scripts/gif.js
+++ public/scripts/gif.js
@@ -15,63 +15,6 @@ $(document).ready(function (){
 
 			for(var i = 0; i<imagesData.length; i++)
 			{
-				imagesData[i].event_link = imageLinks[i];
-			}
-
-			var interval = document.getElementById("interval").value;
-
-			create(imageLinks, interval);
-		}
-	
-
-
-
-	function create(image, int)
-	{
-		gifshot.createGIF({
-	    gifWidth: 200,
-	    gifHeight: 200,
-	    interval: int,
-	    numFrames: 10,
-	    images: image,
-	    frameDuration: 1,
-	    fontWeight: 'normal',
-	    fontSize: '16px',
-	    fontFamily: 'sans-serif',
-	    fontColor: '#ffffff',
-	    textAlign: 'center',
-	    textBaseline: 'bottom',
-	    sampleInterval: 10,
-	    numWorkers: 4
-		}, function (obj) {
-		    if (!obj.error) {
-			var image = obj.image, animatedImage = document.createElement('img');
-			animatedImage.src = image;
-			document.getElementById("gifApp").appendChild(animatedImage);
-			//$("#gif div").appendChild(animatedImage);
-		    }
-		});
-
-	}
-});
-
-
-var imagesData = [];
-var imageLinks = [];
-
-$(document).ready(function (){
-		function getImageArray(data){
-
-			imagesData = [];
-				$("#gif .gifContent").empty();
-				$.each(data, function (index, obj) {
-					if($(obj.event_html).attr("src").indexOf("notcaptured") < 0){
-						imagesData.push(obj);
-					}
-				});
-
-			for(var i = 0; i<imagesData.length; i++)
-			{
 				imageLinks[i] = imagesData[i].event_html;
 				console.log(imageLinks[i]);
 			}
@@ -112,4 +55,5 @@ $(document).ready(function (){
 
 	}
 });
+
 
