376b3a23ae2a1d961f4662c517e65198feebcac5
dhruv282 <40037413+dhruv282@users.noreply.github.com>
2019-04-01 21:55:22 -0400

    timestamp on screenshot

--- public/scripts/gif.js
+++ public/scripts/gif.js
@@ -26,10 +26,77 @@ function getImageArray(data){
 
 function updateGif(){
 	$("#gifApp").empty();
-	var interval = document.getElementById("interval").value;
-	createGif(imageLinks, interval);
+	if(document.getElementById("watermarkOption").checked == true)
+	{
+		watermark();
+	}
+	else{
+		var interval = document.getElementById("interval").value;
+		createGif(imageLinks, interval);
+	}
 }
 	
+function watermark(){
+	
+	var stampedImages = [];
+	for(var i = 0; i<imagesData.length; i++)
+	{
+		var stamp = imagesData[i].event_display_date;
+		var img= new Image();
+		img.src = imageLinks[i];
+		watermarkImage(imageLinks[i],stamp);
+		stampedImages[i] = img.src;
+	}
+	var interval = document.getElementById("interval").value;
+	createGif(stampedImages,interval);
+
+}
+
+
+function watermarkImage(elemImage, text) {
+	var testImage = new Image();
+	testImage.onload = function() {
+		var h = testImage.height, w = testImage.width, img = new Image();
+				   
+		img.onload = function() {
+		     
+			var canvas = Object.assign(document.createElement('canvas'), {width: w, height: h});
+			var ctx = canvas.getContext('2d');
+			ctx.drawImage(testImage, 0, 0);
+			ctx.drawImage(img, 0, 0);
+					      
+			try {
+					elemImage.src = canvas.toDataURL('image/png');
+			}
+			catch (e) {
+					console.error('Cannot watermark image with text:', {src: elemImage.src, text: text, error: e});
+			}
+		};
+				   
+		img.src = 'data:image/svg+xml;base64,' + window.btoa(
+					  '<svg xmlns="http://www.w3.org/2000/svg" height="' + h + '" width="' + w + '">' +
+						  '<foreignObject width="100%" height="100%">' +
+							  '<div xmlns="http://www.w3.org/1999/xhtml">' +
+								  '<div style="position: absolute;' +
+								                        'left: 0;' +
+								                        'bottom: 0;' +
+								                        'font-family: Tahoma;' +
+								                        'font-size: 30pt;' +
+								                        'background: #000;' +
+								                        'color: #fff;' +
+								                        'padding: 0.25em;' +
+								                        'border-radius: 0.25em;' +
+								                        'opacity: 0.6;' +
+								                        'margin: 0 0.125em 0.125em 0;' +
+								   '">' + text.replace(/&/g, "&amp;").replace(/</g, "&lt;").replace(/>/g, "&gt;") + '</div>' +
+							  '</div>' +
+						   '</foreignObject>' +
+					   '</svg>'
+		);
+	};
+		testImage.src = elemImage.src;
+}
+
 
 
 
