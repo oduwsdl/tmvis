b5ceda9ffa0f27b41d15a6017d4aa3810e142baa
a-mabe <39250511+a-mabe@users.noreply.github.com>
2019-04-20 19:14:32 -0400

    Update histogram.js

--- public/scripts/histogram.js
+++ public/scripts/histogram.js
@@ -21,13 +21,13 @@ function getHistogram(dateArray){
 	
 	var endPoint = date.length - 1;
 
-	var fromYear = date[0].event_display_date.substring(0,4);
+	/*var fromYear = date[0].event_display_date.substring(0,4);
 	var fromMonth = date[0].event_display_date.substring(5,2);
 	var fromDate = date[0].event_display_date.substring(8,2);
 	
 	var toYear = date[endPoint].event_display_date.substring(0,4);
 	var toMonth = date[endPoint].event_display_date.substring(5,2);
-	var toDate = date[endPoint].event_display_date.substring(8,2);
+	var toDate = date[endPoint].event_display_date.substring(8,2);*/
 	
 	// set the ranges
 	var x = d3.scaleTime()
