81f15dfd3b067065d3f106bb0cdd51f6e1e7c360
a-mabe <39250511+a-mabe@users.noreply.github.com>
2019-04-21 19:40:05 -0400

    Update timeline.js

--- public/scripts/timeline.js
+++ public/scripts/timeline.js
@@ -836,8 +836,7 @@ function getStats(){
                     
                     //Get the data into an array for the histogram
                     //From and to dates are passed for the domain
-                    getHistoData();
-                    //getHistogram();
+                    getHistoData(fromDate.getFullYear(), fromDate.getMonth(), fromDate.getDate(), toDate.getFullYear(), toDate.getMonth(), toDate.getDate());
                     
                     $(".statsWrapper .collection_stats").html(memStatStr);
 
