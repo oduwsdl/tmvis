ebf6c90e635ab8d0dcee9e0c3507f2e0eaeb4465
a-mabe <39250511+a-mabe@users.noreply.github.com>
2019-04-21 19:26:27 -0400

    Update timeline.js

--- public/scripts/timeline.js
+++ public/scripts/timeline.js
@@ -801,9 +801,9 @@ function getStats(){
         $.ajax({
             type: "GET",
             url: address, // uncomment this for deployment
-            beforeSend: function(xhr) {
+            /*beforeSend: function(xhr) {
               xhr.setRequestHeader("x-my-curuniqueusersessionid",  getUniqueUserSessionId());
-            },
+            },*/
             dataType: "text",
             timeout: 90000000,
             success: function( data, textStatus, jqXHR) {
@@ -831,8 +831,8 @@ function getStats(){
                     var toDate = new Date(jsonObjRes[0].todate);
                     var toDateStr= toDate.getFullYear()+"-"+toDate.getMonth() +"-"+toDate.getDate();
                     var dateRangeStr= fromDateStr + " - " + toDateStr;
-                    $(".statsWrapper .Mementos_Considered").html("TimeMap from "+toDisplay +": "+ jsonObjRes[0]["totalmementos"] +" mementos | "+dateRangeStr);
-                    $(".paraOnlyOnStatsResults").show();*/
+                    $(".statsWrapper .Mementos_Considered").html("TimeMap from "+toDisplay +": "+ jsonObjRes[0]["totalmementos"] +" mementos | "+dateRangeStr);*/
+                    $(".paraOnlyOnStatsResults").show();
                     
                       data = $.trim(data).split("...");
                       if(data.length > 1){
@@ -854,15 +854,15 @@ function getStats(){
                     //getHistoData();
                     //getHistogram();
                     
-                   /* $(".statsWrapper .collection_stats").html(memStatStr);
+                    //$(".statsWrapper .collection_stats").html(memStatStr);
 
 
                     //  $(".statsWrapper .collection_stats").attr("title","Date Range: "+dateRangeStr)
-                    if(  $(".statsWrapper button[type='button']").eq(1).length != 0){
+                    /*if(  $(".statsWrapper button[type='button']").eq(1).length != 0){
                       $(".statsWrapper button[type='button']").eq(1).trigger("click");
                     }else{
                       $(".statsWrapper button[type='button']").eq(0).trigger("click");
-                    }
+                    }*/
 
                     $(".statsWrapper").show();
                     $(".getSummary").show();
@@ -870,7 +870,7 @@ function getStats(){
 
                     //$(".approxTimeShowingPTag").show(800).delay(5000).fadeOut();
                     $(".modal-backdrop").remove();
-                    $('#serverStreamingModal').modal('hide');*/
+                    $('#serverStreamingModal').modal('hide');
 
                 }catch(err){
                     alert($.trim(data));
