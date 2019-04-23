dd486b4be09b5f206d270204c9b170928d802147
a-mabe <39250511+a-mabe@users.noreply.github.com>
2019-04-23 19:11:12 -0400

    Update timeline.js

--- public/scripts/timeline.js
+++ public/scripts/timeline.js
@@ -776,7 +776,7 @@ function setProgressBar(value){
 }
 
 function getStats(){
-  document.getElementById("histoWrapper").style.display = "block";
+  /*document.getElementById("histoWrapper").style.display = "block";
   var collectionIdentifer = $('.argumentsForm #collectionNo').val().trim();
   if(collectionIdentifer == ""){
       collectionIdentifer = "all";
@@ -839,8 +839,8 @@ function getStats(){
           alert(errMsg);
         }
       });
-    }
-  /*var collectionIdentifer = $('.argumentsForm #collectionNo').val();
+    }*/
+  var collectionIdentifer = $('.argumentsForm #collectionNo').val();
   if(collectionIdentifer == ""){
       collectionIdentifer = "all";
   }
@@ -933,7 +933,7 @@ function getStats(){
               // alert(errMsg);
             }
         });
-      }*/
+      }
 }
     
 function getHistoData(){
