01605a2039de78206f07cf29c636c2ae884a0013
a-mabe <mabe.abby.a@gmail.com>
2019-09-15 08:20:50 -0400

    Added "Clear Selection" button for histogram.
    Temporarily removed tool tips.

--- public/scripts/histogram.js
+++ public/scripts/histogram.js
@@ -1,6 +1,4 @@
 function getHistogram(dateArray){
-	var total_selected = 0;
-
 	// set the dimensions and margins of the graph
 	var margin = {top: 10, right: 30, bottom: 30, left: 110},
 	    width = 1000 - margin.left - margin.right,
@@ -21,19 +19,13 @@ function getHistogram(dateArray){
 	    d.date = parseDate(d.event_display_date);
 	});
 
-	var endPoint = data.length - 1;
+	var fromDateString = data[0].event_display_date.substring(0,10);
 
-	var fromYear = data[0].event_display_date.substring(0,4);
-	var fromMonth = data[0].event_display_date.substring(5,7);
-	var fromDate = data[0].event_display_date.substring(8,10);
-	
-	var toYear = data[endPoint].event_display_date.substring(0,4);
-	var toMonth = data[endPoint].event_display_date.substring(5,7);
-	var toDate = data[endPoint].event_display_date.substring(8,10);
+	var toDateString = data[data.length - 1].event_display_date.substring(0,10);
 
 	// get the domain
-	var from = new Date(fromYear, fromMonth, fromDate);
-	var to = new Date(toYear, toMonth, toDate);
+	var from = new Date(fromDateString);
+	var to = new Date(toDateString);
 	
 	// set the ranges
 	var x = d3.scaleTime()
@@ -57,11 +49,6 @@ function getHistogram(dateArray){
 	    .append("g")
 	    .attr("transform", 
 		  "translate(" + margin.left + "," + margin.top + ")");
-	
-	// create tooltip
-	var div = d3.select("body").append("div") 
-		.attr("class", "tooltip")       
-	    .style("opacity", 0);
 
 	// group the data for the bars
 	var bins = histogram(data);
@@ -69,8 +56,19 @@ function getHistogram(dateArray){
 	// Scale the range of the data in the y domain
 	y.domain([0, d3.max(bins, function(d) { return d.length; })]);
 
+	// add the x Axis
+	var xaxis = svg.append("g")
+	    .attr("transform", "translate(0," + height + ")")
+	    .call(d3.axisBottom(x));
+
+	// Create the brush
+	var gBrush = d3.brushX()
+	    .extent([[0,0], [width,height]])
+	    .on("brush", brushed)
+	    .on("end", brushended);
+
 	// append the bar rectangles to the svg element
-	svg.selectAll("rect")
+	var bars = svg.selectAll("rect")
 	    .data(bins)
 	    .enter().append("rect")
 	    .attr("class", "bar")
@@ -79,31 +77,9 @@ function getHistogram(dateArray){
 			return "translate(" + x(d.x0) + "," + y(d.length) + ")"; 
 		})
 	    .attr("width", function(d) { return x(d.x1) - x(d.x0) -1 ; })
-	    .attr("height", function(d) { return height - y(d.length); })
- 	    .on("mouseover", function(d) {    
-		    div .transition()    
-			.duration(200)    
-			.style("opacity", .8);    
-		    div .html("Number of Mementos: " + d.length + "<br/>" + d.x0.toString().substring(4,7) + " " + d.x0.toString().substring(11,15))  
-				.style("left", (d3.event.pageX) + "px")   
-				.style("top", (d3.event.pageY - 28) + "px");  
-		    })          
-	        .on("mouseout", function(d) {   
-			    div .transition()    
-				.duration(500)    
-				.style("opacity", 0); 
-	       });
+	    .attr("height", function(d) { return height - y(d.length); });
 
-	// add the x Axis
-	var bars = svg.append("g")
-	    .attr("transform", "translate(0," + height + ")")
-	    .call(d3.axisBottom(x));
-
-	var gBrush = d3.brushX()
-	    .extent([[0,0], [width,height]])
-	    .on("brush", brushed)
-	    .on("end", brushended);
-
+	// Append the brush
 	svg.append("g")
 	    .attr("class", "brush")
 	    .call(gBrush);
@@ -118,43 +94,20 @@ function getHistogram(dateArray){
 	  	if (!d3.event.sourceEvent) return; // Transition after input
 		if (!d3.event.selection) return; // Ignore when empty
 
+		//document.getElementById("brush_clear").style.display = "block";
+		document.getElementById("brush_clear").style.background = "#337ab7";
+
+		var total_selected = 0;
+
 		var d0 = d3.event.selection.map(x.invert);
 
-	  	d3.selectAll("rect.bar").style("fill", function(d, i) {
-			if (d.x0 >= d0[0] && d.x0 <= d0[1])
-			{
-			    total_selected += d.length;
-			    return "orange";
-			}
-			else
-			{
-			    return "steelblue";
-			}
-		});
+		var total_selected = selectedBars(d0[0], d0[1], total_selected);
 
-	  	// Format from date
-	  	var theFromMonth = d0[0].getMonth() + 1;
-        if(theFromMonth <= 9)
-            theFromMonth = "0" + theFromMonth;
-        var theFromDay = d1[0].getDate();
-        if(theFromDay <= 9)
-        	theFromDay = "0" + theFromDay;
-        // Format to date
-	  	var theToMonth = d0[1].getMonth() + 1;
-        if(theToMonth <= 9)
-            theToMonth = "0" + theToMonth;
-        var theToDay = d1[1].getDate();
-        if(theToDay <= 9)
-        	theToDay = "0" + theToDay;
-
-		var date_selected = d0[0].getFullYear() + "-" + theFromMonth + "-" + theFromDay + " - " + d0[1].getFullYear() + "-" + theToMonth + "-" + theToDay;
-
-		d3.select("#selected_dates").text("Mementos selected: " + total_selected + " mementos " + " | " + date_selected);
-		d3.select("#selected_mementos").text("Selected: " + total_selected);
+		d3.select("#selected_mementos").text("Mementos selected: " + total_selected);
 
 		// Update input values
-		document.getElementById("fromInput").value = d0[0].getFullYear() + "-" + theFromMonth + "-" + d0[0].getDate();
-		document.getElementById("toInput").value = d0[1].getFullYear() + "-" + theToMonth + "-" + d0[1].getDate();
+		document.getElementById("fromInput").value = formatDate(d0[0]);
+		document.getElementById("toInput").value = formatDate(d0[1]);
 
 		total_selected = 0;
 	}
@@ -163,115 +116,165 @@ function getHistogram(dateArray){
 		if (!d3.event.sourceEvent) return; // Transition after input
 		if (!d3.event.selection) return; // Ignore when empty
 
-		var d0 = d3.event.selection.map(x.invert),
-		    d1 = d0.map(d3.timeDay.round);
+		var total_selected = 0;
 
-		// If empty when rounded use floor & ceil
-		if (d1[0] >= d1[1]) {
-		    d1[0] = d3.timeDay.floor(d0[0]);
-		    d1[1] = d3.timeDay.offset(d1[0]);
-		}
+		var d0 = d3.event.selection;
 
 		// Get adjusted from date after brush snap
 		var newFrom = new Date(d1[0].getFullYear(), d1[0].getMonth(), 1, 0,0,0);
-		d1[0] = newFrom;
+		d0[0] = newFrom;
 
 		// Get adjusted to date after brush snap
 		var toDays = new Date(d1[1].getFullYear(), (d1[1].getMonth() + 1), 0).getDate();
 		var newTo = new Date(d1[1].getFullYear(), d1[1].getMonth(), toDays, 11,59,59);
-		d1[1] = newTo;
+		d0[1] = newTo;
 
-		d3.select(this).transition().call(d3.event.target.move, d1.map(x));
+		d3.select(this).transition().call(d3.event.target.move, d0.map(x));
 
-	  	d3.selectAll("rect.bar").style("fill", function(d, i) {
-			if (d.x0 >= d1[0] && d.x0 <= d1[1])
-			{
-			    total_selected += d.length;
-			    return "orange";
-			}
-			else
-			{
-			    return "steelblue";
-			}
-		});
-
-		// Format from date
-	  	var theFromMonth = d1[0].getMonth() + 1;
-        if(theFromMonth <= 9)
-            theFromMonth = "0" + theFromMonth;
-        var theFromDay = d1[0].getDate();
-        if(theFromDay <= 9)
-        	theFromDay = "0" + theFromDay;
-
-        // Format to date
-	  	var theToMonth = d1[1].getMonth() + 1;
-        if(theToMonth <= 9)
-            theToMonth = "0" + theToMonth;
-        var theToDay = d1[1].getDate();
-        if(theToDay <= 9)
-        	theToDay = "0" + theToDay;
-
-		var date_selected = d1[0].getFullYear() + "-" + theFromMonth + "-" + theFromDay + " - " + d1[1].getFullYear() + "-" + theToMonth + "-" + theToDay;
+		var total_selected = selectedBars(d0[0], d0[1], total_selected);
 
 		d3.select("#selected_mementos").text("Mementos selected: " + total_selected);
 
 		// Update input values
-		document.getElementById("fromInput").value = d1[0].getFullYear() + "-" + theFromMonth + "-" + "01";
-		document.getElementById("toInput").value = d1[1].getFullYear() + "-" + theToMonth + "-" + toDays;
+		document.getElementById("fromInput").value = formatDate(d0[0]);
+		document.getElementById("toInput").value = formatDate(d0[1]);
 
 		total_selected = 0;
 	}
 
 	function updateBrush()
 	{
+		// Reset error message
+		document.getElementById('date_error').style.display = "none";
+
 		// Get new date input
-		var fromDate = new Date(document.getElementById("fromInput").value);
-		var toDate = new Date (document.getElementById("toInput").value);
+		var fromDate = document.getElementById("fromInput").value;
+		var toDate = document.getElementById("toInput").value;
 
-		// Adjust dates to snap to bars
-		fromDate = new Date(fromDate.getFullYear(), fromDate.getMonth()+1, 1, 0,0,0);
-		var toDays = new Date(toDate.getFullYear(), (toDate.getMonth() + 1), 0).getDate();
-		toDate = new Date(toDate.getFullYear(), toDate.getMonth(), toDays, 11,59,59);
+		if(isValidDate(fromDate) && isValidDate(toDate))
+		{
+			// Create date objects
+			fromDate = new Date(fromDate);
+			toDate = new Date (toDate);
 
-		// Update brush postion with adjusted user input
-		gBrush.move(d3.select("g.brush").transition().delay(100).duration(100), [fromDate, toDate].map(x));
+			var total_selected = 0;
 
-	  	d3.selectAll("rect.bar").style("fill", function(d, i) {
-			if (d.x0 >= fromDate && d.x0 <= toDate)
+			// Adjust dates to snap to bars
+			fromDate = new Date(fromDate.getFullYear(), fromDate.getMonth(), 1, 0,0,0);
+			var toDays = new Date(toDate.getFullYear(), (toDate.getMonth() + 1), 0).getDate();
+			toDate = new Date(toDate.getFullYear(), toDate.getMonth(), toDays, 11,59,59);
+
+			// Update brush postion with adjusted user input
+			gBrush.move(d3.select("g.brush").transition().delay(100).duration(100), [fromDate, toDate].map(x));
+
+			var total_selected = selectedBars(fromDate, toDate, total_selected);
+
+			d3.select("#selected_mementos").text("Mementos selected: " + total_selected);
+
+			total_selected = 0;
+		}
+		else
+			document.getElementById('date_error').style.display = "block";
+	}
+
+	function selectedBars(from, to, total){
+		d3.selectAll("rect.bar").style("fill", function(d, i) {
+			if (d.x0 >= from && d.x0 <= to)
 			{
-			    total_selected += d.length;
-			    return "orange";
+				total += d.length;
+				return "orange";
 			}
 			else
 			{
-			    return "steelblue";
+				return "steelblue";
 			}
 		});
 
-		// Format from date
-	  	var theFromMonth = fromDate.getMonth() + 1;
-        if(theFromMonth <= 9)
-            theFromMonth = "0" + theFromMonth;
-        var theFromDay = fromDate.getDate();
-        if(theFromDay <= 9)
-        	theFromDay = "0" + theFromDay;
-
-        // Format to date
-	  	var theToMonth = toDate.getMonth() + 1;
-        if(theToMonth <= 9)
-            theToMonth = "0" + theToMonth;
-        var theToDay = toDate.getDate();
-        if(theToDay <= 9)
-        	theToDay = "0" + theToDay;
-
-		var date_selected = fromDate.getFullYear() + "-" + theFromMonth + "-" + theFromDay + " - " + toDate.getFullYear() + "-" + theToMonth + "-" + theToDay;
-
-		d3.select("#selected_mementos").text("Mementos selected: " + total_selected);
-
-		// Update input values
-		document.getElementById("fromInput").value = fromDate.getFullYear() + "-" + theFromMonth + "-" + "01";
-		document.getElementById("toInput").value = toDate.getFullYear() + "-" + theToMonth + "-" + toDays;
-
-		total_selected = 0;
+		return total;
 	}
+
+	function formatDate(date)
+	{
+		var month = date.getMonth() + 1;
+	    if(month <= 9)
+	        month = "0" + month;
+	    var day = date.getDate();
+	    if(day <= 9)
+	        day = "0" + day;
+
+	    return date.getFullYear() + "-" + month + "-" + day;
+	}
+
+	// Validates that the input string is a valid date formatted as "mm/dd/yyyy"
+	function isValidDate(dateString)
+	{
+	    // First check for the pattern
+	    if(!/^\d{4}\-\d{2}\-\d{2}$/.test(dateString))
+	        return false;
+
+	    // Parse the date parts to integers
+	    var parts = dateString.split("-");
+	    var day = parseInt(parts[2], 10);
+	    var month = parseInt(parts[1], 10);
+	    var year = parseInt(parts[0], 10);
+
+	    // Check the ranges of month and year
+	    if(year < 1000 || year > 3000 || month == 0 || month > 12)
+	        return false;
+
+	    var monthLength = [ 31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31 ];
+
+	    // Adjust for leap years
+	    if(year % 400 == 0 || (year % 100 != 0 && year % 4 == 0))
+	        monthLength[1] = 29;
+
+	    // Find index of last memento
+	    var endPoint = histoData.length - 1;
+	    
+	    // Get date of first memento
+	    var fromYear = histoData[0].event_display_date.substring(0,4);
+	    var fromMonth = histoData[0].event_display_date.substring(5,7);
+	    fromMonth = fromMonth - 1;
+	    var fromDay = histoData[0].event_display_date.substring(8,10);
+	    
+	    // Get date of last memento
+	    var toYear = histoData[endPoint].event_display_date.substring(0,4);
+	    var toMonth = histoData[endPoint].event_display_date.substring(5,7);
+	    toMonth = toMonth - 1;
+	    var toDay = histoData[endPoint].event_display_date.substring(8,10);
+	    
+	    month = month - 1;
+	    
+	    // Create date objects
+	    var from = new Date(fromYear, fromMonth, fromDay);
+	    var to = new Date(toYear, toMonth, toDay);
+	    var compareDate = new Date(year, month, day);
+	    
+	    // Check if input within possible range of mementos
+	    if(compareDate < from || compareDate > to){
+	        console.log("out of range");
+	        return false;
+	    }
+	    // Check the range of the day
+	    return day > 0 && day <= monthLength[month];
+	};
+
+	$("#brush_clear").click(function(event){
+		// Reset form input
+		document.getElementById("fromInput").value = fromDateString;
+        document.getElementById("toInput").value = toDateString;
+
+        // Fade clear button
+        document.getElementById("brush_clear").style.background = "#ccc";
+
+		// Reset selected mementos
+        d3.select("#selected_mementos").text("Mementos selected: " + data.length);
+		// Remove brush
+		d3.select("g.brush").call(gBrush.move, null);
+
+		// Reset fill color of each bar
+		d3.selectAll("rect.bar").style("fill", function(d, i) {
+			return "steelblue";
+		});
+	});
 }
